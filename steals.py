c3ecdcd9aafc0e5a0c2c925b9a8cf50170d8b2ba
namishmamidipalli <namishkmamidipalli@gmail.com>
2023-10-25 14:55:50 -0400

    ui updates

--- steals.py
+++ steals.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
-OPTIONS = {
-    'eventfinder': {
-        'name': 'Find Events',
-        'category': '',
-        # 'price': 699,
-    },
-    'trackedevents': {
-        'name': "Events you've been tracking",
-        'category': '',
-        # 'price': 649,
-    }
-}
+
+# Sample data for tracked events and event search results (you will replace this with a database)
+tracked_events = []
+event_search_results = []
+
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', options=OPTIONS)
-@app.route('/option/<key>')
-def option(key):
-    option = OPTIONS.get(key)
-    if not option:
-        abort(404)
-    return render_template('options.html', option=option)
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search_events():
+    location = request.form.get('location')
+    date = request.form.get('date')
+    
+    # Perform the SeatGeek API request here and populate event_search_results
+    
+    return render_template('index.html', event_search_results=event_search_results)
+
+@app.route('/track_event/<event_id>')
+def track_event(event_id):
+    # Add the selected event to the tracked_events list (or your database)
+    
+    return redirect(url_for('index'))
+
+@app.route('/set_threshold', methods=['POST'])
+def set_price_threshold():
+    event_id = request.form.get('event')
+    threshold = float(request.form.get('threshold'))
+    
+    # Set the price threshold for the specified event
+    
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
