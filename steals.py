e691472b69dd32b0be39e1e7edd051e872e5533a
namishmamidipalli <namishkmamidipalli@gmail.com>
2023-10-26 14:32:37 -0400

    added functionality for location and performer

--- steals.py
+++ steals.py
@@ -27,15 +27,24 @@ def search_events():
     
     print(performerapi)
     print(date)
+    print(location)
 
-    url = f'https://api.seatgeek.com/2/performers?slug={performerapi}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
+    url = f'https://api.seatgeek.com/2/events?performers.slug={performerapi}&venue.city={location}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
     req = requests.get(url)
+    #i need to figure out a way to save the postal code here so that i can use it as a parameter for reccomendations if my search doesnt return anything
+
     if req.status_code == 200:
         event_search_results = req.json()
         # print(event_search_results)
-        return render_template('search_results.html', event_search_results=event_search_results)
+        if(len(req.text) > 83):
+            return render_template('search_results.html', event_search_results=event_search_results)
+
     else:
-        return "Error: Unable to retrieve event data"
+        #need to add postal code to url parameters
+        url = f'https://api.seatgeek.com/2/recommendations?venue.city={location}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
+        req = requests.get(url)
+        event_search_results = req.json()
+        return render_template('search_results.html', event_search_results=event_search_results)
 
     # return render_template('index.html', event_search_results=req.json())
 
