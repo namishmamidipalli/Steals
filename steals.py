194a3d5a3d2f5f3a6d6d3ca961c4f0def7943bc1
namishmamidipalli <namishkmamidipalli@gmail.com>
2023-10-25 23:06:14 -0400

    succesful api query

--- steals.py
+++ steals.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -15,11 +16,17 @@ def index():
 def search_events():
     location = request.form.get('location')
     date = request.form.get('date')
-
+    print(date)
     url = f'https://api.seatgeek.com/2/venues?city={location}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
     req = requests.get(url)
-    
-    return render_template('index.html', event_search_results=req.json())
+    if req.status_code == 200:
+        event_search_results = req.json()
+        print(event_search_results)
+        return render_template('search_results.html', event_search_results=event_search_results)
+    else:
+        return "Error: Unable to retrieve event data"
+
+    # return render_template('index.html', event_search_results=req.json())
 
 @app.route('/track_event/<event_id>')
 def track_event(event_id):
