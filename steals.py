8ac2d658e9d09a94530f59a38d2a9cc054a089b5
namishmamidipalli <namishkmamidipalli@gmail.com>
2023-10-26 12:18:53 -0400

    filtered by performer, displays performer results

--- steals.py
+++ steals.py
@@ -16,12 +16,23 @@ def index():
 def search_events():
     location = request.form.get('location')
     date = request.form.get('date')
+    performer = request.form.get('performer')
+    psplit = performer.split()
+    performerapi = ''
+    for i in range(len(psplit)):
+        performerapi += psplit[i]
+        if i + 1 == len(psplit):
+            break
+        performerapi += '-'
+    
+    print(performerapi)
     print(date)
-    url = f'https://api.seatgeek.com/2/venues?city={location}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
+
+    url = f'https://api.seatgeek.com/2/performers?slug={performerapi}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
     req = requests.get(url)
     if req.status_code == 200:
         event_search_results = req.json()
-        print(event_search_results)
+        # print(event_search_results)
         return render_template('search_results.html', event_search_results=event_search_results)
     else:
         return "Error: Unable to retrieve event data"
