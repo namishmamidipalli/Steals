89e7b59efe580de61b25b551146515bfdd186be2
namishmamidipalli <namishkmamidipalli@gmail.com>
2023-10-30 14:40:12 -0400

    improving upon the recommendations feature, specifies artist name and venue name, also provides link to the artists page

--- steals.py
+++ steals.py
@@ -15,6 +15,7 @@ def index():
 @app.route('/search', methods=['POST'])
 def search_events():
     location = request.form.get('location')
+    print(location)
     date = request.form.get('date')
     performer = request.form.get('performer')
     # print(master_performer_list)
@@ -44,12 +45,19 @@ def search_events():
             pid = pid.json()
             pid = pid.get('events')[0].get('performers')[0].get('id')
 
-            recommendations = f'https://api.seatgeek.com/2/recommendations/performers?performers.id={pid}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
+            postal_code = f'https://api.seatgeek.com/2/venues?city={location}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
+            postal_code = requests.get(postal_code)
+            postal_code = postal_code.json()
+            postal_code = postal_code.get('venues')[0].get('postal_code')
+            print(postal_code)
+
+            recommendations = f'https://api.seatgeek.com/2/recommendations?performers.id={pid}&postal_code={postal_code}&client_id=MTUxNTQyNzZ8MTY5ODA3MjA1NS4yMDAxNzYy'
             recommendations = requests.get(recommendations)
             recommendations = recommendations.json()
-            # print(recommendations)
 
-            return render_template('search_results.html', event_search_results=event_search_results, recommendations=recommendations, performer = performer, location = location)
+            print(recommendations)
+
+            return render_template('search_results.html', event_search_results=event_search_results, recommendations=recommendations, artist = performer, location = location)
     else:
         return render_template('index.html', event_search_results=req.json())
 
